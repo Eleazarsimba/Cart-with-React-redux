d10f055d152bd50aa44f91a5575e4f4c03d1372e
Interns <interns@jambopay.com>
2022-09-15 14:10:57 +0300

    lipa

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Home from './components/Home';
 import Cart from './components/Cart';
+import Pagenotfound from './components/Pagenotfound';
 
 // React Notification
 import 'react-notifications/lib/notifications.css';
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />}/>
         <Route exact path="/cart" element={<Cart />}/>
+        <Route path="*" component={<Pagenotfound />} />
       </Routes>
       </BrowserRouter>
       <NotificationContainer />
